6aea8414505bd017d2435e2a4adff7283a12cb71
codewithtan <139702359+codewithtan@users.noreply.github.com>
2023-09-11 01:39:34 +0530

    Add files via upload

---
+++ const_val.py
@@ -0,0 +1,21 @@
+
+width= 400
+height=400
+
+rows= 3
+cols= 3
+square_size= width // cols
+
+line_width= 3
+circ_width= 5
+cross_width = 5
+
+radius = square_size // 4
+
+offset = 50
+
+color = (255, 255, 255)
+bg_color = (153, 255, 204)
+line_color = (0, 0, 0)
+circ_color = (255, 0, 0)
+cross_color = (0, 0, 102)
